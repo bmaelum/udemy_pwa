18a133ed9dbc0bd3cd1cedbf3cb266093408a440
Bjørnar Kjenaas Mælum <bjornarmaelum@gmail.com>
2021-06-20 22:54:02 +0200

    Added more code from the course. Currently at lesson 50.

---
+++ course-project-starting-setup/public/src/js/app.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+    .register('/sw.js')
+    .then(function() {
+        console.log('Service worker registered!');
+    })
+}
+
+window.addEventListener('beforeinstallprompt', function(event))
---
+++ promise-fetch-01--starting-code/public/src/js/app.js
@@ -0,0 +1,32 @@
+
+var deferredPrompt;
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    });
+}
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
+
+var promise = new Promise(function(resolve, reject) {
+  setTimeout(function() {
+    resolve('This is executed once the timer is done!');
+    //console.log('This is executed once the timer is done!');
+  }, 3000);
+})
+
+promise.then(function(text) {
+  console.log(text)
+
+});
+
+
+console.log('This is executed right after setTimeout()');
